d3304c952f02a4a598d9ac47e78d1ee936a5a978
Darren <slimfrog@gmail.com>
2014-02-05 22:24:00 -0800

    Second commit. Testing updates.

--- queries.sql
+++ queries.sql
@@ -1,21 +1,21 @@
-#Select names and departments of all managers.
+#Select names and departments of all managers:
 SELECT first_name,last_name,department1,department2 FROM employee WHERE type="MANAGER";
 
-#Select all male employees.
+#Select all male employees:
 SELECT * FROM employee WHERE gender="MALE";
 
-#Select ID of departments with no manager currently hired.
+#Select ID of departments with no manager currently hired:
 SELECT dep_id FROM department
 WHERE dep_id NOT IN (SELECT department1 FROM employee WHERE type="MANAGER" and termination_date IS NULL)
 OR dep_id NOT IN (SELECT department2 FROM employee WHERE type="MANAGER" and termination_date IS NULL);
 
-#Select salary, first name, last name, and gender of employees currently hired with lowest salary.
+#Select salary, first name, last name, and gender of employees currently hired with lowest salary:
 SELECT salary_amt,first_name,last_name,gender
 FROM salary LEFT JOIN employee
 	ON salary.emp_id=employee.emp_id
 WHERE salary_amt=(SELECT MIN(salary_amt) FROM salary WHERE end_date IS NULL) AND (end_date IS NULL);
 
-#Select name and salary of highest paid employee in "ENGINEERING" who is not a manager.
+#Select name and salary of highest paid employee in "ENGINEERING" who is not a manager:
 SELECT first_name,last_name,salary_amt,department1,department2
 FROM salary RIGHT JOIN employee
 	ON salary.emp_id=employee.emp_id
